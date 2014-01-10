c0e654a3d11592ba704e2ef5948115599eeb43ac
win7 <win7@F200.(none)>
2014-01-10 11:15:36 +0800

    1

---
+++ posthb.js
@@ -0,0 +1,38 @@
+
+var http = require('http');
+
+//console.log(process.argv[2]);
+var server = process.argv[2].split(':');
+var k = process.argv[3];
+var n = process.argv[4];
+var t = process.argv[5];
+var ip = server[0];
+var port = server[1];
+
+if (isNaN(port)){
+	port = 80;
+}else{
+	port = parseInt(port);
+}
+
+console.log(ip);
+console.log(port);
+
+var opt ={
+	host : server,
+	port : port,
+	path : '/post',
+	method : 'POST'
+}
+
+var req = http.request(opt,function(res){
+	res.setEncoding('utf8');
+	res.on('data',function(d){
+		console.log(d);
+	});
+});
+
+req.write(JSON.stringify({key:k,name:n,timeout:t}));
+
+//req.on('error',function(e){
+req.end();
